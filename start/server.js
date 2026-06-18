ae03c15a8cc76b240de1bb5ea050977475541a38
Shivam... <shivam243028@gmail.com>
2026-06-18 12:43:20 +0530

    Backend start

--- start/server.js
+++ start/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/test",(req , res)=>{
+app.checkoutget("/test",(req , res)=>{
     res.send("ok me tumhara server hu")
 })
 
 app.listen(3000,()=>{
-    console.log("server is running on path 3000")
+    console.log("server is running on port 3000")
 })
