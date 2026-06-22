68140db075ee7c0f42628abbe0d9ee7999adf462
Shivam... <shivam243028@gmail.com>
2026-06-22 18:17:22 +0530

    api

---
+++ Rest_Api/server.js
@@ -0,0 +1,5 @@
+// GET    => Server se data fetch (read) karne ke liye use hota hai.
+// POST   => Server par naya data send/create karne ke liye use hota hai.
+// PATCH  => Existing data ka kuch specific part update karne ke liye use hota hai.
+// DELETE => Existing data ko delete karne ke liye use hota hai.
+// PUT    => Existing data ko completely update/replace karne ke liye use hota hai.
