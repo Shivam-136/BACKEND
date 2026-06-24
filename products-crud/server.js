f1e84d259251847eb25b484e8f0ad2b6b705fd8b
Shivam... <shivam243028@gmail.com>
2026-06-24 16:13:20 +0530

    make api

---
+++ products-crud/server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./src/config/db");
+
+const ProductModel = require("./src/models/product.model");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// Get All Products
+app.get("/api/product", async (req, res) => {
+  try {
+    let allProducts = await ProductModel.find();
+
+    return res.status(200).json({
+      success: true,
+      message: "All products fetched",
+      data: allProducts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+// Update Product
+app.patch("/api/product/update/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const {
+      productName,
+      description,
+      category,
+      imageUrl,
+      currency,
+      amount,
+    } = req.body;
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      productId,
+      {
+        productName,
+        description,
+        category,
+        imageUrl,
+        price: {
+          currency,
+          amount,
+        },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+// Get Single Product
+app.get("/api/product/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const product = await ProductModel.findById(productId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Product fetched",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+// Delete Product
+app.delete("/api/product/delete/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    await ProductModel.findByIdAndDelete(productId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
