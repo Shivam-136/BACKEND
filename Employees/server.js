a96e5f27cf27e29a8b5650b6bcb70c28a5c8165c
Shivam... <shivam243028@gmail.com>
2026-06-26 13:07:56 +0530

    Employee-crud

--- Employees/server.js
+++ Employees/server.js
@@ -1,21 +1,21 @@
-import express from "express"
-import { connectdb } from "./src/config/db.js"
-import cors from "cors"
+import express from "express";
+import { connectDb } from "./src/config/db.js";
+import employeeRoutes from "./src/routes/employee.routes.js";
+import cors from "cors";
 
-connectdb()
-const app = express()
+connectDb();
+const app = express();
 
 app.use(
-    cors({
-        origin:"http://localhost:5173"
-    })
-)
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("api/employees")
+app.use("/api/employees", employeeRoutes);
 
-
-app.listen(3000,() => {
-    console.log("server is running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
