892370196dd583181818bf10019d5bb9ffddec6d
Shivam... <shivam243028@gmail.com>
2026-07-07 11:58:47 +0530

    Auth-Backend

--- Auth/server.js
+++ Auth/server.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./src/config/db");
 const userRoutes = require("./src/routes/user.routes");
+// const homeRoutes = require("./src/routes/home.routes");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 connectDB();
 
@@ -13,6 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+// app.use("/api/home", homeRoutes);
 
 app.listen(3000, () => {
   console.log("running on port 3000");
